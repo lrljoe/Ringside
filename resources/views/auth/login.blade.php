632bcf37b8e37f3cda902de4c884bef56285acb6
Jeffrey Davidson <jeffrey.davidson@outlook.com>
2019-05-02 09:07:43 -0400

    Feature/login (#24)
    * Add login assets
    
    * Add login view
    
    * Add layout for authentication

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,73 +1,79 @@
-@extends('layouts.app')
+@extends('layouts.auth')
+
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+<div class="kt-login__body">
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
+    <!--begin::Signin-->
+    <div class="kt-login__form">
+        <div class="kt-login__title">
+            <h3>Sign In</h3>
+        </div>
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+        <!--begin::Form-->
+        <form class="kt-form" action="{{ route('login') }}" novalidate="novalidate">
+            <div class="form-group">
+                <input class="form-control" type="text" placeholder="Email" name="email" autocomplete="off">
+                @error('email')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                @enderror
+            </div>
+            <div class="form-group">
+                <input class="form-control" type="password" placeholder="Password" name="password">
+                @error('password')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                @enderror
+            </div>
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
-
-                                @if ($errors->has('email'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
+            <!--begin::Action-->
+            @if (Route::has('password.request'))
+                <div class="kt-login__actions">
+                    <a href="#" class="kt-link kt-login__link-forgot">
+                        Forgot Password ?
+                    </a>
+                    <button id="kt_login_signin_submit" class="btn btn-primary btn-elevate kt-login__btn-primary">{{ __('Login') }}</button>
                 </div>
+            @endif
+
+            <!--end::Action-->
+        </form>
+
+        <!--end::Form-->
+
+        <!--begin::Divider-->
+        <div class="kt-login__divider">
+            <div class="kt-divider">
+                <span></span>
+                <span>OR</span>
+                <span></span>
             </div>
         </div>
+
+        <!--end::Divider-->
+
+        <!--begin::Options-->
+        <div class="kt-login__options">
+            <a href="#" class="btn btn-primary kt-btn">
+                <i class="fab fa-facebook-f"></i>
+                Facebook
+            </a>
+            <a href="#" class="btn btn-info kt-btn">
+                <i class="fab fa-twitter"></i>
+                Twitter
+            </a>
+            <a href="#" class="btn btn-danger kt-btn">
+                <i class="fab fa-google"></i>
+                Google
+            </a>
+        </div>
+
+        <!--end::Options-->
     </div>
+
+    <!--end::Signin-->
 </div>
 @endsection
