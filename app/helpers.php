696b29c913d5dd03dc14411a86e22e2ef87272b3
Jeffrey Davidson <jeffrey.davidson@outlook.com>
2019-05-03 15:27:35 -0400

    Add function to keep parent menu item open (#34)

--- app/helpers.php
+++ app/helpers.php
@@ -4,3 +4,8 @@ function set_active($name, $active = 'kt-menu__item--active')
 {
     return Route::currentRouteNamed($name) ? $active : '';
 }
+
+function set_open(array $routes, $open = 'kt-menu__item--open kt-menu__item--here')
+{
+    return in_array(Route::currentRouteName(), $routes) ? $open : '';
+}
--- resources/views/layouts/partials/aside.blade.php
+++ resources/views/layouts/partials/aside.blade.php
@@ -53,7 +53,7 @@
                     <h4 class="kt-menu__section-text">Administrator</h4>
                     <i class="kt-menu__section-icon flaticon-more-v2"></i>
                 </li>
-                <li class="kt-menu__item  kt-menu__item--submenu" aria-haspopup="true" data-ktmenu-submenu-toggle="hover">
+                <li class="kt-menu__item kt-menu__item--submenu kt-menu__item kt-menu__item--submenu {{ set_open(['wrestlers.index', 'tagteams.index', 'managers.index', 'referees.index', 'stables.index']) }}" aria-haspopup="true" data-ktmenu-submenu-toggle="hover">
                     <a href="javascript:;" class="kt-menu__link kt-menu__toggle">
                         <i class="kt-menu__link-icon flaticon-users-1"></i>
                         <span class="kt-menu__link-text">Roster</span>
