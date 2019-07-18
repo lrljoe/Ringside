453a2e96f0e6a7f95468d7a7ef6f1e7415a70a52
Jeffrey Davidson <jeffrey.davidson@outlook.com>
2019-07-18 01:02:46 -0400

    Add partial for searching (#69)

---
+++ resources/views/partials/search.blade.php
@@ -0,0 +1,13 @@
+<div class="kt-subheader__group" id="kt_subheader_search">
+    <span class="kt-subheader__desc" id="kt_subheader_total"></span>
+    <form class="kt-margin-l-20" id="kt_subheader_search_form">
+        <div class="kt-input-icon kt-input-icon--right kt-subheader__search">
+            <input type="text" class="form-control" placeholder="Search..." id="generalSearch">
+            <span class="kt-input-icon__icon kt-input-icon__icon--right">
+                <span>
+                    <i class="flaticon2-search-1"></i>
+                </span>
+            </span>
+        </div>
+    </form>
+</div>
