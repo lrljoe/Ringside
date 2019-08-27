a046137ce5de76c856637e8cabf1d84504712c34
Jeffrey Davidson <jeffrey.davidson@outlook.com>
2019-08-27 19:33:40 -0400

    Refactor to blade service provider (#102)
    * Refactor to traits
    
    * Add status to models
    
    * Remove status accessor
    
    * Add model observers
    
    * Remove toArray override
    
    * Add relationships for polymorphic models.
    
    * Fix existing scopes
    
    * Move blade components to a service component

---
+++ app/Providers/BladeServiceProvider.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\ServiceProvider;
+
+class BladeServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        Blade::component('components.filter', 'filter');
+        Blade::component('components.flatpickr', 'flatpickr');
+        Blade::component('components.statusSelect', 'statusSelect');
+        Blade::component('components.buttons.activate', 'activatebutton');
+        Blade::component('components.buttons.delete', 'deletebutton');
+        Blade::component('components.buttons.edit', 'editbutton');
+        Blade::component('components.buttons.injure', 'injurebutton');
+        Blade::component('components.buttons.recover', 'recoverbutton');
+        Blade::component('components.buttons.reinstate', 'reinstatebutton');
+        Blade::component('components.buttons.retire', 'retirebutton');
+        Blade::component('components.buttons.suspend', 'suspendbutton');
+        Blade::component('components.buttons.unretire', 'unretirebutton');
+        Blade::component('components.buttons.view', 'viewbutton');
+    }
+}
