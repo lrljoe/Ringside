1072218d7f5fe81ffa40339c7eeb4fbfca22a98e
Jefffrey Davidson <jeffrey.davidson@outlook.com>
2019-12-17 20:45:55 -0500

    Remove duplicated blade components

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,6 @@
 namespace App\Providers;
 
 use Spatie\BladeX\Facades\BladeX;
-use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
 use App\Http\Requests\CustomDataTablesRequest;
 
@@ -32,19 +31,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Blade::component('components.filter', 'filter');
-        Blade::component('components.flatpickr', 'flatpickr');
-        Blade::component('components.statusSelect', 'statusSelect');
-        Blade::component('components.buttons.activate', 'activatebutton');
-        Blade::component('components.buttons.delete', 'deletebutton');
-        Blade::component('components.buttons.edit', 'editbutton');
-        Blade::component('components.buttons.injure', 'injurebutton');
-        Blade::component('components.buttons.recover', 'recoverbutton');
-        Blade::component('components.buttons.reinstate', 'reinstatebutton');
-        Blade::component('components.buttons.retire', 'retirebutton');
-        Blade::component('components.buttons.suspend', 'suspendbutton');
-        Blade::component('components.buttons.unretire', 'unretirebutton');
-        Blade::component('components.buttons.view', 'viewbutton');
         BladeX::component('components.*');
     }
 }
