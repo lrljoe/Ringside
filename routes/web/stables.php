173586a4302dc5ca5ffd382e76249c9858902a58
Jeffrey Davidson <jeffrey.davidson@outlook.com>
2020-07-08 12:23:52 -0400

    Cleanup stable routes

--- routes/web/stables.php
+++ routes/web/stables.php
@@ -1,16 +1,18 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Stables\StablesController;
+use App\Http\Controllers\Stables\ActivateController;
+use App\Http\Controllers\Stables\DeactivateController;
+use App\Http\Controllers\Stables\DisassembleController;
 use App\Http\Controllers\Stables\RestoreController;
 use App\Http\Controllers\Stables\RetireController;
+use App\Http\Controllers\Stables\StablesController;
 use App\Http\Controllers\Stables\UnretireController;
-use App\Http\Controllers\Stables\ActivateController;
-use App\Http\Controllers\Stables\DisassembleController;
+use Illuminate\Support\Facades\Route;
 
 Route::resource('stables', StablesController::class);
 Route::put('/roster/stables/{stable}/restore', RestoreController::class)->name('stables.restore');
 Route::put('/roster/stables/{stable}/retire', RetireController::class)->name('stables.retire');
 Route::put('/roster/stables/{stable}/unretire', UnretireController::class)->name('stables.unretire');
 Route::put('/roster/stables/{stable}/activate', ActivateController::class)->name('stables.activate');
+Route::put('/roster/stables/{stable}/deactivate', DeactivateController::class)->name('stables.deactivate');
 Route::put('/roster/stables/{stable}/disassemble', DisassembleController::class)->name('stables.disassemble');
