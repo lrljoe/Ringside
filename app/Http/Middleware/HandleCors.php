a434b82d54bb26939ab41170168d512a60bde7e9
Jeffrey Davidson <jdavidsonwebdev@gmail.com>
2022-01-26 22:06:46 -0500

    Update class name to reflect file name

--- app/Http/Middleware/HandleCors.php
+++ app/Http/Middleware/HandleCors.php
@@ -4,7 +4,7 @@ namespace App\Http\Middleware;
 
 use Fruitcake\Cors\HandleCors as Middleware;
 
-class HandleCORS extends Middleware
+class HandleCors extends Middleware
 {
     /**
      * The paths to enable CORS on.
