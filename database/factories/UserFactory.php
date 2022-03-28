e315c7f624e1044d7f7fe602d27d4af41211408d
Jeffrey Davidson <jdavidsonwebdev@gmail.com>
2022-03-28 12:07:20 -0400

    Fix Failing Auth Tests (#151)
    * Remove unneeded features and corresponding tests
    
    * Use plain text with password with getting bcrypted in backend

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -28,7 +28,7 @@ class UserFactory extends Factory
             'last_name' => $this->faker->lastName(),
             'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
+            'password' => 'secret',
             'remember_token' => Str::random(10),
             'role' => Role::basic(),
         ];
