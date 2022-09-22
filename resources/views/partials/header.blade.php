3c92080cbd7324e6a1a2dd06be98adac18a9f829
Jeffrey Davidson <jdavidsonwebdev@gmail.com>
2022-09-22 08:37:26 -0400

    Change which avatar is shown by default (#180)

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -26,7 +26,7 @@
                 <div class="app-navbar-item ms-1 ms-lg-3" id="kt_header_user_menu_toggle">
                     <!--begin::Menu wrapper-->
                     <div class="cursor-pointer symbol symbol-35px symbol-md-40px" data-kt-menu-trigger="click" data-kt-menu-attach="parent" data-kt-menu-placement="bottom-end">
-                        <img src="{{ asset('assets/media/avatars/300-1.jpg') }}" alt="{{ auth()->user()->first_name }} {{ auth()->user()->last_name }}">
+                        <img src="{{ asset('assets/media/avatars/blank.png') }}" alt="{{ auth()->user()->first_name }} {{ auth()->user()->last_name }}">
                     </div>
                     <!--begin::User account menu-->
                     <div class="py-4 menu menu-sub menu-sub-dropdown menu-column menu-rounded menu-gray-800 menu-state-bg menu-state-color fw-semibold fs-6 w-275px" data-kt-menu="true">
@@ -35,7 +35,7 @@
                             <div class="px-3 menu-content d-flex align-items-center">
                                 <!--begin::Avatar-->
                                 <div class="symbol symbol-50px me-5">
-                                    <img alt="{{ auth()->user()->first_name }} {{ auth()->user()->last_name }}" src="{{ asset('assets/media/avatars/300-1.jpg') }}">
+                                    <img alt="{{ auth()->user()->first_name }} {{ auth()->user()->last_name }}" src="{{ asset('assets/media/avatars/blank.png') }}">
                                 </div>
                                 <!--end::Avatar-->
                                 <!--begin::Username-->
