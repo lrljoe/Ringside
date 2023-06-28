02ae94c18f073fe0000f129db8babbef6be2dc7f
Jeffrey Davidson <jdavidsonwebdev@gmail.com>
2023-06-28 13:32:10 -0400

    Update docblock

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -36,7 +36,7 @@ class EventServiceProvider extends ServiceProvider
     /**
      * The model observers for your application.
      *
-     * @var array
+     * @var  array<string, array<int, object|string>|object|string>
      */
     protected $observers = [
         Event::class => [EventObserver::class],
