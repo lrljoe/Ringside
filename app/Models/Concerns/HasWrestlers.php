1500f69d4ab31496336dca2408e47b6433dc2a88
Jeffrey Davidson <jdavidsonwebdev@gmail.com>
2023-07-06 20:38:53 -0400

    Move common logic to a trait

---
+++ app/Models/Concerns/HasWrestlers.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Models\Concerns;
+
+use App\Models\Wrestler;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+trait HasWrestlers
+{
+    /**
+     * Get the wrestlers that have been tag team partners of the tag team.
+     */
+    public function wrestlers(): BelongsToMany
+    {
+        return $this->belongsToMany(Wrestler::class, 'tag_team_wrestler')
+            ->withPivot('joined_at', 'left_at');
+    }
+
+    /**
+     * Get current wrestlers of the tag team.
+     *
+     * @return BelongsToMany<Wrestler>
+     */
+    public function currentWrestlers(): BelongsToMany
+    {
+        return $this->wrestlers()
+            ->wherePivotNull('left_at');
+    }
+
+    /**
+     * Get previous tag team partners of the tag team.
+     */
+    public function previousWrestlers(): BelongsToMany
+    {
+        return $this->wrestlers()
+            ->wherePivotNotNull('left_at');
+    }
+
+    /**
+     * Get the combined weight of both tag team partners in a tag team.
+     */
+    public function combinedWeight(): Attribute
+    {
+        return new Attribute(
+            get: fn () => $this->currentWrestlers->sum('weight')
+        );
+    }
+}
--- app/Models/TagTeam.php
+++ app/Models/TagTeam.php
@@ -14,10 +14,8 @@ use App\Models\Contracts\Manageable;
 use App\Models\Contracts\Retirable;
 use App\Models\Contracts\Suspendable;
 use Fidum\EloquentMorphToOne\HasMorphToOne;
-use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\MorphToMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
@@ -65,43 +63,6 @@ class TagTeam extends Model implements Bookable, CanBeAStableMember, Competitor,
     public function newEloquentBuilder($query): TagTeamQueryBuilder
     {
         return new TagTeamQueryBuilder($query);
-    }
-
-    /**
-     * Get the wrestlers that have been tag team partners of the tag team.
-     */
-    public function wrestlers(): BelongsToMany
-    {
-        return $this->belongsToMany(Wrestler::class, 'tag_team_wrestler')
-            ->withPivot('joined_at', 'left_at');
-    }
-
-    /**
-     * Get current wrestlers of the tag team.
-     */
-    public function currentWrestlers(): BelongsToMany
-    {
-        return $this->wrestlers()
-            ->wherePivotNull('left_at');
-    }
-
-    /**
-     * Get previous tag team partners of the tag team.
-     */
-    public function previousWrestlers(): BelongsToMany
-    {
-        return $this->wrestlers()
-            ->wherePivotNotNull('left_at');
-    }
-
-    /**
-     * Get the combined weight of both tag team partners in a tag team.
-     */
-    public function combinedWeight(): Attribute
-    {
-        return new Attribute(
-            get: fn () => $this->currentWrestlers->sum('weight')
-        );
     }
 
     /**
