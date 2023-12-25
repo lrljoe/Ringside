df50a496254af9c5707fb2e3c8c4543476019910
Jeffrey Davidson <jdavidsonwebdev@gmail.com>
2023-12-25 01:58:28 -0500

    Fix referee forms (#230)
    * Remove extra file
    
    * Refactor to a form partial

--- resources/views/referees/edit.blade.php
+++ resources/views/referees/edit.blade.php
@@ -14,46 +14,15 @@
         </x-toolbar>
     </x-slot>
 
-    <div class="shadow-sm card">
-        <div class="card-header">
-            <h3 class="card-title">Edit Referee Form</h3>
-        </div>
-        <div class="card-body">
-            <form method="post" action="{{ route('referees.update', $referee) }}">
-                @method('patch')
-                @csrf
-                <div class="mb-10">
-                    <div class="mb-5 row gx-10">
-                        <div class="col-lg-6">
-                            <x-form.inputs.text
-                                label="First Name:"
-                                name="first_name"
-                                placeholder="First Name Here"
-                                :value="old('first_name', $referee->first_name)"
-                            />
-                        </div>
-                        <div class="col-lg-6">
-                            <x-form.inputs.text
-                                label="Last Name:"
-                                name="last_name"
-                                placeholder="Last Name Here"
-                                :value="old('last_name', $referee->last_name)"
-                            />
-                        </div>
-                    </div>
-                </div>
-                <div class="mb-10">
-                    <x-form.inputs.date
-                        label="Start Date:"
-                        name="start_date"
-                        :value="old('start_date', $referee->started_at?->format('Y-m-d'))"
-                    />
-                </div>
-        </div>
-        <div class="card-footer">
-            <x-form.buttons.submit />
-            <x-form.buttons.reset />
-        </div>
-        </form>
-    </div>
+    <x-card>
+        <x-slot name="header">
+            <x-card.header title="Edit Referee Form" />
+        </x-slot>
+        <x-card.body>
+            <x-form :action="route('referees.update', $referee)">
+                @method('PATCH')
+                @include('referees.partials.form')
+            </x-form>
+        </x-card.body>
+    </x-card>
 </x-layouts.app>
