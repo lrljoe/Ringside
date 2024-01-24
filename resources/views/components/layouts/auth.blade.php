ffa3889bdbfedc9a49e6471d0c7f32ee24628434
Jeffrey Davidson <jdavidsonwebdev@gmail.com>
2024-01-24 10:36:29 -0500

    Use vite to call style sheets

--- resources/views/components/layouts/auth.blade.php
+++ resources/views/components/layouts/auth.blade.php
@@ -6,16 +6,14 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <title>{{ config('app.name', 'Laravel') }}</title>
+        
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700"/>
 
-        <!--begin::Fonts-->
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" />
-        <!--end::Fonts-->
-        <!--begin::Global Stylesheets Bundle(used by all pages)-->
-        <link href="{{ asset('assets/plugins/global/plugins.bundle.css') }}" rel="stylesheet" type="text/css" />
-        <link href="{{ asset('assets/css/style.bundle.css') }}" rel="stylesheet" type="text/css" />
-        <!--end::Global Stylesheets Bundle-->
+        @vite('resources/css/app.css')
+        @vite('resources/css/plugins.bundle.css')
+        @vite('resources/css/style.bundle.css')
     </head>
-	<!-- end::Head -->
+    <!-- end::Head -->
 
 	<!-- begin::Body -->
 	<body class="kt-quick-panel--right kt-demo-panel--right kt-offcanvas-panel--right kt-header--fixed kt-header-mobile--fixed kt-subheader--fixed kt-subheader--enabled kt-subheader--solid kt-aside--enabled kt-aside--fixed kt-page--loading">
