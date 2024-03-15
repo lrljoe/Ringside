83da898db57127557b215ddb97f92df5438700c9
Jeffrey Davidson <3826104+JeffreyDavidson@users.noreply.github.com>
2024-03-15 16:45:30 -0400

    Fixing the login view (#289)

--- resources/views/components/layouts/auth.blade.php
+++ resources/views/components/layouts/auth.blade.php
@@ -1,68 +1,169 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
-        
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700"/>
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
-        @vite('resources/css/app.css')
-        @vite('resources/css/plugins.bundle.css')
-        @vite('resources/css/style.bundle.css')
-    </head>
-    <!-- end::Head -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700"/>
 
-	<!-- begin::Body -->
-	<body class="kt-quick-panel--right kt-demo-panel--right kt-offcanvas-panel--right kt-header--fixed kt-header-mobile--fixed kt-subheader--fixed kt-subheader--enabled kt-subheader--solid kt-aside--enabled kt-aside--fixed kt-page--loading">
+    @vite('resources/css/app.css')
+    @vite('resources/css/plugins.bundle.css')
+    @vite('resources/css/style.bundle.css')
+</head>
+<!-- end::Head -->
 
-		<div class="d-flex flex-column flex-column-fluid bgi-position-y-bottom position-x-center bgi-no-repeat bgi-size-contain bgi-attachment-fixed" style="background-image: url(assets/media/illustrations/sketchy-1/14.png">
+<body id="kt_body" class="app-blank bgi-size-cover bgi-attachment-fixed bgi-position-center" cz-shortcut-listen="true"
+      data-new-gr-c-s-check-loaded="8.911.0" data-gr-ext-installed="">
+<!--begin::Theme mode setup on page load-->
+<script>
+    var defaultThemeMode = "light";
+    var themeMode;
+
+    if (document.documentElement) {
+        if (document.documentElement.hasAttribute("data-bs-theme-mode")) {
+            themeMode = document.documentElement.getAttribute("data-bs-theme-mode");
+        } else {
+            if (localStorage.getItem("data-bs-theme") !== null) {
+                themeMode = localStorage.getItem("data-bs-theme");
+            } else {
+                themeMode = defaultThemeMode;
+            }
+        }
+
+        if (themeMode === "system") {
+            themeMode = window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light";
+        }
+
+        document.documentElement.setAttribute("data-bs-theme", themeMode);
+    }
+</script>
+<!--end::Theme mode setup on page load-->
+<!--Begin::Google Tag Manager (noscript) -->
+<noscript>
+    <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5FS8GGP" height="0" width="0"
+            style="display:none;visibility:hidden"></iframe>
+</noscript>
+<!--End::Google Tag Manager (noscript) -->
+
+<!--begin::Root-->
+<div class="d-flex flex-column flex-root" id="kt_app_root">
+    <!--begin::Page bg image-->
+    <style>
+        body {
+            background-image: url('/assets/media/auth/bg10.jpeg');
+        }
+
+        [data-bs-theme="dark"] body {
+            background-image: url('/assets/media/auth/bg10-dark.jpeg');
+        }
+    </style>
+    <!--end::Page bg image-->
+
+    <!--begin::Authentication - Sign-in -->
+    <div class="d-flex flex-column flex-lg-row flex-column-fluid">
+        <!--begin::Aside-->
+        <div class="d-flex flex-lg-row-fluid">
             <!--begin::Content-->
-            <div class="p-10 d-flex flex-center flex-column flex-column-fluid pb-lg-20">
-                {{ $slot }}
+            <div class="d-flex flex-column flex-center pb-0 pb-lg-10 p-10 w-100">
+                <!--begin::Image-->
+                <img class="theme-light-show mx-auto mw-100 w-150px w-lg-300px mb-10 mb-lg-20"
+                     src="{{ asset('assets/media/auth/agency.png') }}" alt="">
+                <img class="theme-dark-show mx-auto mw-100 w-150px w-lg-300px mb-10 mb-lg-20"
+                     src="{{ asset('assets/media/auth/agency-dark.png') }}" alt="">
+                <!--end::Image-->
+
+                <!--begin::Title-->
+                <h1 class="text-gray-800 fs-2qx fw-bold text-center mb-7">
+                    Fast, Efficient and Productive
+                </h1>
+                <!--end::Title-->
+
+                <!--begin::Text-->
+                <div class="text-gray-600 fs-base text-center fw-semibold">
+                    In this kind of post, <a href="#" class="opacity-75-hover text-primary me-1">the blogger</a>
+
+                    introduces a person they’ve interviewed <br> and provides some background information about
+
+                    <a href="#" class="opacity-75-hover text-primary me-1">the interviewee</a>
+                    and their <br> work following this is a transcript of the interview.
+                </div>
+                <!--end::Text-->
             </div>
             <!--end::Content-->
         </div>
+        <!--begin::Aside-->
 
-		<!-- begin::Global Config(global config for global JS sciprts) -->
-		{{-- <script>
-			var KTAppOptions = {
-				"colors": {
-					"state": {
-						"brand": "#5d78ff",
-						"dark": "#282a3c",
-						"light": "#ffffff",
-						"primary": "#5867dd",
-						"success": "#34bfa3",
-						"info": "#36a3f7",
-						"warning": "#ffb822",
-						"danger": "#fd3995"
-					},
-					"base": {
-						"label": ["#c5cbe3", "#a1a8c3", "#3d4465", "#3e4466"],
-						"shape": ["#f0f3ff", "#d9dffa", "#afb4d4", "#646c9a"]
-					}
-				}
-			};
-		</script> --}}
+        <!--begin::Body-->
+        <div class="d-flex flex-column-fluid flex-lg-row-auto justify-content-center justify-content-lg-end p-12">
+            <!--begin::Wrapper-->
+            <div class="bg-body d-flex flex-column flex-center rounded-4 w-md-600px p-10">
+                <!--begin::Content-->
+                <div class="d-flex flex-center flex-column align-items-stretch h-lg-100 w-md-400px">
+                    <!--begin::Wrapper-->
+                    <div class="d-flex flex-center flex-column flex-column-fluid pb-15 pb-lg-20">
 
-		<!-- end::Global Config -->
+                        <!--begin::Form-->
+                        <form class="form w-100 fv-plugins-bootstrap5 fv-plugins-framework" novalidate="novalidate"
+                              id="kt_sign_in_form" method="post" action="{{ route('login') }}">
+                            @csrf
+                            <!--begin::Heading-->
+                            <div class="text-center mb-11">
+                                <!--begin::Title-->
+                                <h1 class="text-gray-900 fw-bolder mb-3">
+                                    Sign In
+                                </h1>
+                                <!--end::Title-->
+                            </div>
+                            <!--begin::Heading-->
 
-		<!--begin::Javascript-->
-		<script>var hostUrl = "assets/";</script>
-		<!--begin::Global Javascript Bundle(used by all pages)-->
-		<script src="{{ asset('assets/plugins/global/plugins.bundle.js') }}"></script>
-		<script src="{{ asset('assets/js/scripts.bundle.js') }}"></script>
-		<!--end::Global Javascript Bundle-->
-		<!--begin::Page Custom Javascript(used by this page)-->
-		<script src="{{ asset('assets/js/widgets.bundle.js') }}"></script>
-		<script src="{{ asset('assets/js/custom/widgets.js') }}"></script>
-		<script src="{{ asset('assets/js/button-ajax.js') }}"></script>
-		<!--end::Page Custom Javascript-->
-		<!--end::Javascript-->
-	</body>
+                            <!--begin::Input group--->
+                            <div class="fv-row mb-8 fv-plugins-icon-container">
+                                <input type="text" placeholder="Email" name="email" autocomplete="off"
+                                       value="{{ old('email') }}"
+                                       class="form-control bg-transparent">
+                                <div
+                                    class="fv-plugins-message-container fv-plugins-message-container--enabled invalid-feedback"></div>
+                            </div>
 
-	<!-- end::Body -->
+                            <div class="fv-row mb-3 fv-plugins-icon-container">
+                                <input type="password" placeholder="Password" name="password" autocomplete="off"
+                                       class="form-control bg-transparent">
+                                <div
+                                    class="fv-plugins-message-container fv-plugins-message-container--enabled invalid-feedback"></div>
+                            </div>
+
+                            <div class="d-flex flex-stack flex-wrap gap-3 fs-base fw-semibold mb-8">
+                                <div></div>
+
+                                <a href="/metronic8/demo1/authentication/layouts/overlay/reset-password.html"
+                                   class="link-primary">
+                                    Forgot Password ?
+                                </a>
+                            </div>
+
+                            <div class="d-grid mb-10">
+                                <button type="submit" id="kt_sign_in_submit" class="btn btn-primary mb-4">
+                                    <span class="indicator-label">Sign In</span>
+                                </button>
+                            </div>
+                        </form>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<svg id="SvgjsSvg1001" width="2" height="0" xmlns="http://www.w3.org/2000/svg" version="1.1"
+     xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:svgjs="http://svgjs.dev"
+     style="overflow: hidden; top: -100%; left: -100%; position: absolute; opacity: 0;">
+    <defs id="SvgjsDefs1002"></defs>
+    <polyline id="SvgjsPolyline1003" points="0,0"></polyline>
+    <path id="SvgjsPath1004" d="M0 0 "></path>
+</svg>
+</body>
 </html>
