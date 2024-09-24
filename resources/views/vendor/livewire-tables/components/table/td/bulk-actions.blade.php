f2d2dffbf9a88536f5f2b1625fd07afaf9be4903
Jeffrey Davidson <3826104+JeffreyDavidson@users.noreply.github.com>
2024-09-24 13:38:08 -0400

    Add more cleanup to livewire tables components

--- resources/views/vendor/livewire-tables/components/table/td/bulk-actions.blade.php
+++ resources/views/vendor/livewire-tables/components/table/td/bulk-actions.blade.php
@@ -14,6 +14,7 @@
             'form-check' => $theme === 'bootstrap-5',
         ])>
             <input
+                class="checkbox checkbox-sm"
                 x-cloak x-show="!currentlyReorderingStatus"
                 x-model="selectedItems"
                 wire:key="{{ $tableName . 'selectedItems-'.$row->{$primaryKey} }}"
@@ -21,11 +22,7 @@
                 value="{{ $row->{$primaryKey} }}"
                 type="checkbox"
                 {{
-                    $attributes->merge($bulkActionsTdCheckboxAttributes)->class([
-                        'rounded border-gray-300 text-indigo-600 shadow-sm transition duration-150 ease-in-out focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 dark:bg-gray-900 dark:text-white dark:border-gray-600 dark:hover:bg-gray-600 dark:focus:bg-gray-600' => ($theme === 'tailwind') && ($bulkActionsTdCheckboxAttributes['default'] ?? true),
-                        'form-check-input' => ($theme === 'bootstrap-5') && ($bulkActionsTdCheckboxAttributes['default'] ?? true),
-                        'except' => 'default',
-                    ])
+                    $attributes->merge($bulkActionsTdCheckboxAttributes)
                 }}
             />
         </div>
