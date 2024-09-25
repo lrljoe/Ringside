4fe3f375e6942b9f67b4d6dece85f57ba10a027e
Jeffrey Davidson <3826104+JeffreyDavidson@users.noreply.github.com>
2024-09-25 13:16:03 -0400

    Add functionality to open and close a menu item if it has a sub menu (#313)

--- resources/views/components/menu/menu-item.blade.php
+++ resources/views/components/menu/menu-item.blade.php
@@ -1,11 +1,16 @@
-@props(['icon' => 'ki-element-11', 'isOpen' => true])
+@props(['icon' => 'ki-element-11'])
 
-<div @class([
-    'menu-item',
-    'menu-item-accordian' => isset($subMenu),
-    'here show' => $isOpen,
-])>
-    <div class="menu-link flex items-center grow cursor-pointer border border-transparent gap-[10px] pl-[10px] pr-[10px] py-[6px]"
+<div x-data="{
+    open: false,
+    toggle() {
+        this.open = this.open ? this.close() : true
+    },
+    close() {
+        this.open = false
+    }
+}" @class(['menu-item', 'menu-item-accordian' => isset($subMenu)])>
+    <div @click="toggle()"
+        class="menu-link flex items-center grow cursor-pointer border border-transparent gap-[10px] pl-[10px] pr-[10px] py-[6px]"
         href="" tabindex="0">
         <span class="menu-icon items-start text-gray-500 dark:text-gray-400 w-[20px]">
             <i class="ki-filled {{ $icon }} text-lg"></i>
@@ -16,16 +21,15 @@
         </span>
         @if (isset($subMenu))
             <span class="menu-arrow text-gray-400 w-[20px] shrink-0 justify-end ml-1 mr-[-10px]">
-                <i class="ki-filled ki-plus text-2xs menu-item-show:hidden"></i>
-                <i class="ki-filled ki-minus text-2xs hidden menu-item-show:inline-flex"></i>
+                <i x-show="!open" class="ki-filled ki-plus text-2xs menu-item-show:hidden"></i>
+                <i x-show="open" class="ki-filled ki-minus text-2xs menu-item-show:inline-flex"></i>
             </span>
         @endif
     </div>
     @if (isset($subMenu))
-        <div @class([
+        <div :class="open ? 'show' : 'hidden'" x-show="open" @class([
             'menu-accordion gap-0.5 pl-[10px] relative before:absolute before:left-[20px] before:top-0 before:bottom-0 before:border-l before:border-gray-200',
-            'show' => $isOpen,
-        ])>
+        ]) style="display: none;">
             {{ $subMenu }}
         </div>
     @endif
