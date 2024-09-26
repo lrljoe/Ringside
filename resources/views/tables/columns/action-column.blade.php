3ef09f6150fcceca4c2eb1dc652642fa72d021f4
Jeffrey Davidson <3826104+JeffreyDavidson@users.noreply.github.com>
2024-09-26 19:03:57 -0400

    WIP Working on Dropdown for actions menu

--- resources/views/tables/columns/action-column.blade.php
+++ resources/views/tables/columns/action-column.blade.php
@@ -1,9 +1,31 @@
-<div class="menu">
-    <div class="menu-item menu-item-dropdown">
-        <button class="menu-toggle btn btn-sm btn-icon btn-light btn-clear">
+<div class="menu" x-data="{
+    open: false,
+    toggle() {
+        if (this.open) {
+            return this.close()
+        }
+
+        this.$refs.button.focus()
+
+        this.open = true
+    },
+    close(focusAfter) {
+        if (!this.open) return
+
+        this.open = false
+
+        focusAfter && focusAfter.focus()
+    }
+}" x-on:keydown.escape.prevent.stop="close($refs.button)"
+    x-on:focusin.window="! $refs.panel.contains($event.target) && close()" x-id="['dropdown-button']">
+    <div class="menu-item menu-item-dropdown" :class="open ? 'show' : ''">
+        <button x-ref="button" x-on:click="toggle()" :aria-expanded="open" :aria-controls="$id('dropdown-button')"
+            class="menu-toggle btn btn-sm btn-icon btn-light btn-clear">
             <i class="ki-filled ki-dots-vertical"></i>
         </button>
-        <div class="menu-dropdown menu-default w-full max-w-[175px] show">
+        <div class="menu-dropdown menu-default w-full max-w-[175px]" x-ref="panel" x-show="open"
+            x-transition.origin.top.left x-on:click.outside="close($refs.button)" :id="$id('dropdown-button')"
+            :class="open ? 'show' : 'hidden'">
             @if ($links['view'] ?? true)
                 <div class="menu-item">
                     <a href="{{ route($path . '.show', $rowId) }}">
