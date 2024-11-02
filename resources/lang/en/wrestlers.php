f81ed650771c89c313c342bdd6271e83e73eedc5
Jeffrey Davidson <3826104+JeffreyDavidson@users.noreply.github.com>
2024-11-01 21:40:35 -0400

    Add language files

---
+++ resources/lang/en/wrestlers.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'name' => 'Name',
+    'height' => 'Height',
+    'weight' => 'Weight',
+    'signature_move' => 'Signature Move',
+    'hometown' => 'Hometown',
+    'status' => 'Status',
+];
