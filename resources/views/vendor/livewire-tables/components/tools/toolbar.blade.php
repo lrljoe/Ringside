6ffb4bcc5c043207f8daba0038d11abbe1787809
Jeffrey Davidson <3826104+JeffreyDavidson@users.noreply.github.com>
2024-11-22 20:48:23 -0500

    Remove extra div (#380)

--- resources/views/vendor/livewire-tables/components/tools/toolbar.blade.php
+++ resources/views/vendor/livewire-tables/components/tools/toolbar.blade.php
@@ -1,44 +1,46 @@
-@aware(['component', 'tableName','isTailwind','isBootstrap'])
+@aware(['component', 'tableName', 'isTailwind', 'isBootstrap'])
 @props([])
 
+@if ($this->reorderIsEnabled())
+    <x-livewire-tables::tools.toolbar.items.reorder-buttons />
+@endif
 
-    @if ($this->reorderIsEnabled())
-        <x-livewire-tables::tools.toolbar.items.reorder-buttons />
-    @endif
+@if ($this->filtersAreEnabled() && $this->filtersVisibilityIsEnabled() && $this->hasVisibleFilters())
+    <x-livewire-tables::tools.toolbar.items.filter-button />
+@endif
 
-    @if ($this->filtersAreEnabled() && $this->filtersVisibilityIsEnabled() && $this->hasVisibleFilters())
-        <x-livewire-tables::tools.toolbar.items.filter-button />
-    @endif
+@if ($this->hasActions && $this->showActionsInToolbar && $this->getActionsPosition == 'left')
+    <x-livewire-tables::includes.actions />
+@endif
 
-    @if($this->hasActions && $this->showActionsInToolbar && $this->getActionsPosition == 'left')
-        <x-livewire-tables::includes.actions/>
-    @endif
+@if ($this->hasConfigurableAreaFor('toolbar-right-start'))
+    @include(
+        $this->getConfigurableAreaFor('toolbar-right-start'),
+        $this->getParametersForConfigurableArea('toolbar-right-start'))
+@endif
 
-    @if ($this->hasConfigurableAreaFor('toolbar-right-start'))
-        @include($this->getConfigurableAreaFor('toolbar-right-start'), $this->getParametersForConfigurableArea('toolbar-right-start'))
-    @endif
+@if ($this->hasActions && $this->showActionsInToolbar && $this->getActionsPosition == 'right')
+    <x-livewire-tables::includes.actions />
+@endif
 
-    @if($this->hasActions && $this->showActionsInToolbar && $this->getActionsPosition == 'right')
-        <x-livewire-tables::includes.actions/>
-    @endif
+@if ($this->showBulkActionsDropdownAlpine() && $this->shouldAlwaysHideBulkActionsDropdownOption != true)
+    <x-livewire-tables::tools.toolbar.items.bulk-actions />
+@endif
 
-    @if ($this->showBulkActionsDropdownAlpine() && $this->shouldAlwaysHideBulkActionsDropdownOption != true)
-        <x-livewire-tables::tools.toolbar.items.bulk-actions />
-    @endif
+@if ($this->columnSelectIsEnabled())
+    <x-livewire-tables::tools.toolbar.items.column-select />
+@endif
 
-    @if ($this->columnSelectIsEnabled())
-        <x-livewire-tables::tools.toolbar.items.column-select />
-    @endif
+@if ($this->hasConfigurableAreaFor('toolbar-right-end'))
+    @include(
+        $this->getConfigurableAreaFor('toolbar-right-end'),
+        $this->getParametersForConfigurableArea('toolbar-right-end'))
+@endif
 
-    @if ($this->hasConfigurableAreaFor('toolbar-right-end'))
-        @include($this->getConfigurableAreaFor('toolbar-right-end'), $this->getParametersForConfigurableArea('toolbar-right-end'))
-    @endif
-</div>
 @if (
     $this->filtersAreEnabled() &&
-    $this->filtersVisibilityIsEnabled() &&
-    $this->hasVisibleFilters() &&
-    $this->isFilterLayoutSlideDown()
-)
-    <x-livewire-tables::tools.toolbar.items.filter-slidedown  />
+        $this->filtersVisibilityIsEnabled() &&
+        $this->hasVisibleFilters() &&
+        $this->isFilterLayoutSlideDown())
+    <x-livewire-tables::tools.toolbar.items.filter-slidedown />
 @endif
