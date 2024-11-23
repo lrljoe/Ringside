a032dbc75e65b9ba24362bd80346021d3886b49c
Jeffrey Davidson <3826104+JeffreyDavidson@users.noreply.github.com>
2024-11-22 21:17:50 -0500

    Remove all unused views and components (#381)

--- resources/views/components/actions-dropdown.blade.php
+++
@@ -1,8 +0,0 @@
-<a href="#" class="btn btn-light btn-active-light-primary btn-flex btn-center btn-sm" data-kt-menu-trigger="click" data-kt-menu-placement="bottom-end">
-    Actions
-    <i class="ki-duotone ki-down fs-5 ms-1"></i>
-</a>
-
-<div class="menu menu-sub menu-sub-dropdown menu-column menu-rounded menu-gray-600 menu-state-bg-light-primary fw-semibold fs-7 w-125px py-4" data-kt-menu="true" style="z-index: 107; position: fixed; inset: 0px 0px auto auto; margin: 0px; transform: translate3d(-132px, 365px, 0px);" data-popper-placement="bottom-end">
-    {{ $slot }}
-</div>
