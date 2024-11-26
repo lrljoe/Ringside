f937e466e739d6946ca4e657a6911609c68a54fc
Jeffrey Davidson <3826104+JeffreyDavidson@users.noreply.github.com>
2024-11-25 21:45:44 -0500

    Add primary color to menu links (#386)

--- resources/views/components/mega-menu/menu-link.blade.php
+++ resources/views/components/mega-menu/menu-link.blade.php
@@ -3,7 +3,7 @@
 ])
 
 <a {{ $attributes }} @class([
-    'flex items-center grow cursor-pointer text-sm',
+    'flex items-center grow cursor-pointer text-sm group-hover:text-primary',
     'text-gray-900 font-medium' => $isActive,
     'text-gray-800' => !$isActive,
 ])>
