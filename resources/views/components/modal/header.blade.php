3cdf85442f622a368aa470fa7a8d4e83bff3afa6
Jeffrey Davidson <3826104+JeffreyDavidson@users.noreply.github.com>
2025-01-08 14:10:50 -0500

    wip

--- resources/views/components/modal/header.blade.php
+++ resources/views/components/modal/header.blade.php
@@ -2,7 +2,11 @@
     style="padding-block-start:.625rem;padding-block-end:.625rem">
     <h3 class="text-md font-semibold text-gray-900">{{ $this->getModalTitle() }}</h3>
     <button
-        class="inline-flex items-center cursor-pointer leading-none rounded-md size-8 ps-3 pe-3 font-medium text-xs gap-0 p-0 justify-center border-transparent bg-transparent text-gray-700 hover:bg-gray-200 hover:border-transparent hover:shadow-none hover:text-gray-800" wire:click="$dispatch('closeModal')">
+        class="inline-flex items-center cursor-pointer leading-none rounded-md size-8 ps-3 pe-3 font-medium text-xs gap-0 p-0 justify-center border-transparent bg-transparent text-gray-700 hover:bg-gray-200 hover:border-transparent hover:shadow-none hover:text-gray-800"
+        wire:click="$dispatch('closeModal')">
         <i class="ki-outline ki-cross text-gray-600 text-xl"></i>
     </button>
+    <x-buttons.light size="xs" wire:click="$dispatch('closeModal')">
+        <i class="ki-outline ki-cross text-gray-600 text-xl"></i>
+    </x-buttons.light>
 </div>
