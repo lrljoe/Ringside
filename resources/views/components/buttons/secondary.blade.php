3fc899279e4d86281959ff300f83afbf5da72974
Jeffrey Davidson <3826104+JeffreyDavidson@users.noreply.github.com>
2025-01-08 18:56:03 -0500

    wip

--- resources/views/components/buttons/secondary.blade.php
+++ resources/views/components/buttons/secondary.blade.php
@@ -4,7 +4,7 @@
 
 <x-button
     {{ $attributes->merge()->class([
-            'text-gray-700 bg-light border-gray-300 hover:bg-light-active hover:text-gray-800 hover:border-gray-300 hover:shadow-default active:bg-light-active active:text-gray-800 active:border-gray-300 active:shadow-default focus:bg-light-active focus:text-gray-800 focus:border-gray-300 focus:shadow-default',
+            'text-gray-700 bg-secondary border-gray-200 hover:bg-light-active hover:text-gray-800 hover:border-gray-300 hover:shadow-default active:bg-light-active active:text-gray-800 active:border-gray-300 active:shadow-default focus:bg-light-active focus:text-gray-800 focus:border-gray-300 focus:shadow-default',
         ]) }}>
     {{ $slot }}
 </x-button>
--- resources/views/components/buttons/warning.blade.php
+++ resources/views/components/buttons/warning.blade.php
@@ -4,7 +4,7 @@
 
 <x-button
     {{ $attributes->merge()->class([
-            'text-white bg-warning hover:bg-warning-active hover:shadow-warning-box-shadow active:bg-warning-active active:shadow-warning-box-shadow focusr:bg-warning-active focus:shadow-warning-box-shadow',
+            'text-white bg-warning hover:bg-warning-active hover:shadow-warning active:bg-warning-active active:shadow-warning-box-shadow focusr:bg-warning-active focus:shadow-warning-box-shadow',
         ]) }}>
     {{ $slot }}
 </x-button>
