0559b25fd7e542cf2a40cd611f4387425992bdfe
Jeffrey Davidson <3826104+JeffreyDavidson@users.noreply.github.com>
2025-01-10 18:56:55 -0500

    update

--- app/Livewire/Concerns/BaseModal.php
+++ app/Livewire/Concerns/BaseModal.php
@@ -4,33 +4,34 @@ declare(strict_types=1);
 
 namespace App\Livewire\Concerns;
 
-use Exception;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Log;
 use Illuminate\View\View;
 use LivewireUI\Modal\ModalComponent;
 
 class BaseModal extends ModalComponent
 {
-    protected ?Model $model;
+    public ?Model $model;
 
     protected string $modelTitleField = 'name';
 
     public function mount(?int $modelId = null): void
     {
         if (isset($modelId) && ! is_null($modelId)) {
-            $this->model = $this->modelType->find($modelId);
-            $this->form->setModel($this->model);
+            try {
+                $this->model = $this->modelType::findOrFail($modelId);
+                $this->modelForm->setModel($this->model);
+            } catch (\Exception $e) {
+                Log::error($e->getMessage());
+            }
         }
     }
 
     public function getModalTitle(): string
     {
-        if (isset($this->model)) {
-            if (property_exists($this->model, $this->modelTitleField)) {
-                return 'Edit '.$this->model->{$this->modelTitleField};
-            }
 
-            throw new Exception('Property '.$this->modelTitleField.' does not exist on the model.');
+        if (isset($this->modelForm) && isset($this->modelForm->formModel)) {
+            return 'Edit '.($this->modelForm->formModel->{$this->modelTitleField} ?? 'Unknown');
         }
 
         return 'Add '.class_basename($this->modelType);
